94ed137cd87a71bb2ae773e30c597916f1d373aa
Eric O'Callaghan <eric@ericoc.com>
2024-12-01 19:10:33 -0500

    better logging in convert

--- convert.py
+++ convert.py
@@ -14,41 +14,48 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# Read and parse new CSV file data into a dictionary.
-new_days = {}
-with open(file="ChartData.csv", mode="r", encoding="utf-8") as csv_fh:
+# File names.
+FILENAMES = {"csv": "ChartData.csv", "json": "water.json"}
+logger.debug("Files:\t%s" % FILENAMES)
+
+# Create dictionary from (new) CSV data.
+new = {}
+with open(file=FILENAMES["csv"], mode="r", encoding="utf-8") as csv_fh:
     for csv_row in DictReader(csv_fh):
+        assert csv_row[" Units"] == " Gallons", "Invalid unit."
         date_parts = csv_row[" Time Interval"].strip().split('/')
         date_iso = f"{date_parts[2]}-{date_parts[0]}-{date_parts[1]}"
         water_date = date.fromisoformat(date_iso)
-        new_days[water_date] = float(csv_row[" Consumption"].strip())
-logger.info(f"CSV:\t{len(new_days)}")
+        new[water_date] = float(csv_row[" Consumption"].strip())
+    logger.info("New CSV:\t%i (%s)" % (len(new), FILENAMES["csv"]))
 
-# Read and parse existing JSON file data into a dictionary.
-existing_days = {}
-with open(file="water.json", mode="r", encoding="utf-8") as existing_fh:
+# Create dictionary from (existing) JSON data.
+existing = {}
+with open(file=FILENAMES["json"], mode="r", encoding="utf-8") as existing_fh:
     existing_usage = json.load(existing_fh)
     for existing_row in existing_usage:
         existing_date = date.fromisoformat(existing_row["date"])
-        existing_days[existing_date] = existing_row["gallons"]
-logger.info(f"Existing:\t{len(existing_days)}")
+        existing[existing_date] = existing_row["gallons"]
+    logger.info("Existing:\t%i (%s)" % (len(existing), FILENAMES["json"]))
 
-# Merge dictionaries of new CSV data with existing JSON data.
-merged_days = new_days | existing_days
-logger.info(f"Merged:\t{len(merged_days)}")
+# Merge dictionaries of new and existing data.
+merged = new | existing
 
-# Create a list of the merged data dictionaries.
-merged_usage = []
-for day, gallons in merged_days.items():
-    merged_usage.append({
-        "date": day.isoformat(),
-        "gallons": gallons
-    })
+# Create, and sort, a list of the merged data dictionaries.
+usage = []
+for day, gallons in merged.items():
+    usage.append({"date": day.isoformat(), "gallons": gallons})
+usage = sorted(usage, key=lambda i: i["date"])
 
-# Sort the list, by date, of all merged data.
-merged_usage = sorted(merged_usage, key=lambda i: i["date"])
-logger.info(f"Max:\t{merged_usage[-1]['date']}")
-logger.info(f"Min:\t{merged_usage[0]['date']}")
-with open(file="water.json", mode="w", encoding="utf-8") as merged_fh:
-    json.dump(merged_usage, merged_fh, indent=4)
+# Display minimum and maximum dates from the merged list.
+dt_fmt = "%A, %B %d, %Y"
+min_date = date.fromisoformat(usage[0]["date"])
+logger.info('Minimum:\t%s (%s)' % (min_date.strftime(dt_fmt), min_date))
+max_date = date.fromisoformat(usage[-1]["date"])
+logger.info('Maximum:\t%s (%s)' % (max_date.strftime(dt_fmt), max_date))
+
+# Write the list of merged data to existing JSON file.
+with open(file=FILENAMES["json"], mode="w", encoding="utf-8") as merged_fh:
+    json.dump(usage, merged_fh, indent=4)
     merged_fh.write("\n")
+    logger.info("Done:\t%i (%s)" % (len(usage), FILENAMES["json"]))
